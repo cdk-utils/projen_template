04389342a50d5e55022b2b27b35977dafbeb8b87
Lorenzo Hidalgo Gadea <lorenzo@lhidalgo.dev>
2025-12-05 10:22:13 +0100

    chore: updated projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { awscdk } from "projen";
+import { awscdk, ReleasableCommits } from "projen";
 import { JobPermission } from "projen/lib/github/workflows-model";
 
 const project = new awscdk.AwsCdkConstructLibrary({
@@ -6,20 +6,47 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: "lorenzo@lhidalgo.dev",
   cdkVersion: "2.1.0",
   defaultReleaseBranch: "main",
+  releasableCommits: ReleasableCommits.featuresAndFixes(),
   jsiiVersion: "~5.9.0",
   name: "projen_template",
   projenrcTs: true,
   repositoryUrl: "https://github.com/cdk-utils/projen_template.git",
+  eslint: false,
+  prettier: false,
+  biome: true,
+  stale: true,
+  githubOptions: {
+    pullRequestLintOptions: {
+      semanticTitleOptions: {
+        types: ["feat", "fix", "docs", "chore"],
+      },
+    },
+  },
+});
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+project.release!.publisher.publishToNpm({
+  registry: "npm.pkg.github.com",
 });
 
 const autoApprove = project.github!.addWorkflow("admin-auto-approve");
 
-autoApprove.on({ pullRequestTarget: {} });
+autoApprove.on({
+  pullRequestTarget: {},
+  workflowDispatch: {
+    inputs: {
+      logLevel: {
+        description: "Log level",
+        required: true,
+        default: "warning",
+      },
+      environment: {
+        description: "Environment to deploy",
+        required: false,
+        default: "staging",
+      },
+    },
+  },
+});
 autoApprove.addJobs({
   autoApprove: {
     runsOn: ["ubuntu-latest"],
@@ -30,7 +57,7 @@ autoApprove.addJobs({
     steps: [
       {
         uses: "actions-ecosystem/action-add-labels@v1",
-        with: { "labels": "do-not-merge" },
+        with: { labels: "do-not-merge" },
       },
       {
         uses: "hmarr/auto-approve-action@v4",
